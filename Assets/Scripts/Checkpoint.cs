c74337d3c4a9a178d212e02b2a3b9a5173c4f8e5
FeelingHxllxw <malchenko.03@mail.ru>
2023-05-19 16:34:55 +0500

    Чекпоинты
    Нужно как то поправить скрипт чекпоинта чтобы он работал

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -2,17 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Checkpoiny : MonoBehaviour
+public class Checkpoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (collision.transform.tag == "Player")
+        {
+            LevelManager.respawnPoint.position = transform.position;
+        }
     }
 }
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class LevelManager : MonoBehaviour
 {
     public static LevelManager instance;
-    public Transform respawnPoint;
+    public static Transform respawnPoint;
     public GameObject playerPrefab;
 
     private void Awake()
