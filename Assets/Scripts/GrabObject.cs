1bb437ace8a2c1b4df58910c46d4e07e80306399
FeelingHxllxw <malchenko.03@mail.ru>
2023-05-24 19:46:39 +0500

    Скрипт Поднятия предмета

---
+++ Assets/Scripts/GrabObject.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabObject : MonoBehaviour
+{
+    [SerializeField]
+    private Transform grabPoint;
+
+    [SerializeField]
+    private Transform rayPoint;
+
+    [SerializeField]
+    private float rayDistance;
+
+    private GameObject grabbedObject;
+    private int layerIndex;
+    void Start()
+    {
+        layerIndex = LayerMask.NameToLayer("Objects");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(rayPoint.position, transform.right, rayDistance);
+        if (hitInfo.collider != null && hitInfo.collider.gameObject.layer == layerIndex)
+        {
+            if (Input.GetKeyDown(KeyCode.E) && grabbedObject == null)
+            {
+                grabbedObject = hitInfo.collider.gameObject;
+                grabbedObject.GetComponent<Rigidbody2D>().isKinematic = true;
+                grabbedObject.transform.position = grabPoint.position;
+                grabbedObject.transform.SetParent(transform);
+            }
+            else if (Input.GetKeyDown(KeyCode.E))
+            {
+                grabbedObject.GetComponent<Rigidbody2D>().isKinematic = false;
+                grabbedObject.transform.SetParent(null);
+                grabbedObject = null;
+            }
+        }
+
+        Debug.DrawRay(rayPoint.position, transform.right * rayDistance);
+    }
+}
